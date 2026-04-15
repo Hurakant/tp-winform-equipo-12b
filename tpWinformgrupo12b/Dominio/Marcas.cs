ce22d74db9af32997e0a158834bcefe6674ccbf8
Antonio <mosqueraantonio2003@gmail.com>
2026-04-15 19:41:02 -0300

    completé los datos de las demas clases

--- tpWinformgrupo12b/Dominio/Marcas.cs
+++ tpWinformgrupo12b/Dominio/Marcas.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace tpWinformgrupo12b
 {
-    internal class Marcas
+    public class Marca
     {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        // Sobreescribimos para que los ComboBox muestren el nombre y no la ruta del objeto
+        public override string ToString()
+        {
+            return Descripcion;
+        }
     }
 }
