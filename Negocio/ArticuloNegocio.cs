ef3b93455ff8154e2c251e2dfd75027d56751b77
Antonio <mosqueraantonio2003@gmail.com>
2026-04-15 23:42:17 -0300

    Se añadió la clase ArticuloNegocio y se desarrolló

---
+++ Negocio/ArticuloNegocio.cs
@@ -0,0 +1,47 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> Lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsulta("Server=localhost,1433; Database=CATALOGO_P3_DB; integrated security=false; User Id=sa; Password=Antonio12345@; TrustServerCertificate=True");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (Int32)datos.Lector["Id"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Marca = (string)datos.Lector["Marca"];
+                    aux.Categoria = (string)datos.Lector["IdCategoria"];
+                    aux.Imagen = (string)datos.Lector["ImagenUrl"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+
+                    Lista.Add(aux);
+                }
+                return Lista;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { datos.cerrarConexion(); }
+        }
+
+
+    }
+}
